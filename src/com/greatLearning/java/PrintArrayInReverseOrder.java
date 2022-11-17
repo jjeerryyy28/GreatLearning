a08bbe8126742d9979f8ba89f2add4e816cec70c
Prajwal Nikhar <prajwalnikhar1@gmail.com>
2022-11-18 00:55:33 +0530

    Printing array in reverse order

---
+++ src/com/greatLearning/java/PrintArrayInReverseOrder.java
@@ -0,0 +1,17 @@
+package com.greatLearning.java;
+
+import java.util.Scanner;
+
+public class PrintArrayInReverseOrder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<=n-1;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
